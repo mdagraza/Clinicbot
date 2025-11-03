ad69e7e800cdaa729f510d6530f827128e31e21b
Miguel Dagraza <migueldagraza@gmail.com>
2025-11-03 15:57:55 +0100

    feat: Hacer clase singleton para las llamadas a la base de datos

--- db_connection.py
+++ db_connection.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from threading import Lock
 
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
@@ -73,3 +74,34 @@ class MongoDBConnection_Usuarios:
         
     def get_collection(self, collection_name):
         return self.db[collection_name]
+
+class MongoDBConnection:
+    _instance = None
+    _lock = Lock()
+
+    def __new__(cls):
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super().__new__(cls)
+                cls._instance.client = MongoClient(MONGODB_HOST, MONGODB_PORT)
+        return cls._instance
+    
+    def get_collection_db_muestras(self, collection: str = "muestras"): #Se pone por defecto la colección que mas se usa, si se necesita otra, se define en la llamada
+        self.db = self.client["Clinicbot-Muestras"]
+        return self.db[collection]
+    
+    def get_collection_db_petri(self, collection: str = "petri"):
+        self.db = self.client["Clinicbot-Petri"]
+        return self.db[collection]
+    
+    def get_collection_db_pacientes(self, collection: str = "pacientes"):
+        self.db = self.client["Clinicbot-Pacientes"]
+        return self.db[collection]
+    
+    def get_collection_db_log(self, collection: str = "access"):
+        self.db = self.client["Clinicbot-Logs"]
+        return self.db[collection]
+    
+    def get_collection_db_usuarios(self, collection: str = "usuarios"):
+        self.db = self.client["Clinicbot-Usuarios"]
+        return self.db[collection]
