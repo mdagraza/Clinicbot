16566f95de04547486c94e1f98995806b3be98d0
Miguel Dagraza <migueldagraza@gmail.com>
2025-11-04 10:49:10 +0100

    refactor: Sacar importaciones repetidas de funciones y hacerlas locales

--- Funciones/Generales.py
+++ Funciones/Generales.py
@@ -1,7 +1,7 @@
-def peticion_datos(user_id, peticion):
-    import requests
-    from django.conf import settings
+import requests
+from django.conf import settings
 
+def peticion_datos(user_id, peticion):
     # Obtener la lista de la peticion a través de API REST
     api_url = f"{settings.API_URL}/api/{peticion}"
     headers = {
@@ -16,9 +16,6 @@ def peticion_datos(user_id, peticion):
         return []
     
 def peticion_datos_detalle(user_id, peticion, id):
-    import requests
-    from django.conf import settings
-
     # Obtener la lista de la peticion a través de API REST
     api_url = f"{settings.API_URL}/api/{peticion}/{id}" 
     headers = {
