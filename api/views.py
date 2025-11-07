ed07535dcc0c907dd5184e12d879bf4a1044fef1
AnxoBenavides04 <164198552+AnxoBenavides04@users.noreply.github.com>
2025-11-07 12:47:43 +0100

    refactor: Descomantada la validacion del POST para petri

--- api/views.py
+++ api/views.py
@@ -198,13 +198,13 @@ class ItemListView_Petri(APIView):
     def post(self, request):
         data = request.data
 
-        '''#Filtrar entrada del codigo
+        #Filtrar entrada del codigo
         if "identificacion" not in data or not re.match(PATRON_CODE, data["identificacion"]):
             return Response({"error": "La identificacion(AAAA.123456789) no es válida."}, status=status.HTTP_400_BAD_REQUEST)
         
         # Buscar si ya existe en la base de datos
         if self.collection.find_one({"identificacion": data["identificacion"]}):
-            return Response({"error": f"Ya existe un registro con la identificacion: {str(data['identificacion'])}"}, status=status.HTTP_400_BAD_REQUEST)'''
+            return Response({"error": f"Ya existe un registro con la identificacion: {str(data['identificacion'])}"}, status=status.HTTP_400_BAD_REQUEST)
 
         # Agregar la fecha
         data["fecha_recepcion"] = datetime.now().strftime("YYYY.mm.ddTHH:mm")
